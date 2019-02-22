c23b57194d48bebc5f8b0b720a10849e7b3eea40
feikaixin <954630886@qq.com>
2019-02-22 16:01:19 +0800

    修改格式

--- react-router/index.js
+++ react-router/index.js
@@ -1,69 +1,65 @@
 import React, {Component} from 'react'
-import {
-    Route,
-    Link,
-    jsHistory
-} from './router'
+import { Route, Link, jsHistory } from './router'
 
 const App = () => (
-    <div>
-        <ul className="nav">
-            <li><Link to="/">Home</Link></li>
-            <li><Link to="/about">About</Link></li>
-            <li><Link to="/topics">Topics</Link></li>
-        </ul>
+  <div>
+    <ul className="nav">
+      <li><Link to="/">Home</Link></li>
+      <li><Link to="/about">About</Link></li>
+      <li><Link to="/topics">Topics</Link></li>
+    </ul>
 
-        <BtnHome/>
-        <BtnAbout/>
-        <BtnTopics/>
-        <hr/>
+    <BtnHome/>
+    <BtnAbout/>
+    <BtnTopics/>
+    <hr/>
 
-        <Route exact path="/" component={Home}/>
-        <Route path="/about" component={About}/>
-        <Route path="/topics" component={Topics}/>
-    </div>
+    <Route exact path="/" component={Home}/>
+    <Route path="/about" component={About}/>
+    <Route path="/topics" component={Topics}/>
+  </div>
 );
 
 const Home = () => (
-    <div>
-        <h2>Home</h2>
-    </div>
+  <div>
+    <h2>Home</h2>
+  </div>
 );
 
 const About = () => (
-    <div>
-        <h2>About</h2>
-    </div>
+  <div>
+    <h2>About</h2>
+  </div>
 );
 
 const Topics = ({match}) => (
-    <div>
-        <h2>Topics</h2>
-    </div>
+  <div>
+    <h2>Topics</h2>
+  </div>
 );
 
 class BtnHome extends Component {
-    render() {
-        return (
-            <button onClick={jsHistory.pushState.bind(this, '/')}>Home</button>
-        )
-    }
+  render() {
+    return (
+      <button onClick={jsHistory.pushState.bind(this, '/')}>Home</button>
+    )
+  }
 }
 
 class BtnAbout extends Component {
-    render() {
-        return (
-            <button onClick={jsHistory.pushState.bind(this, '/about')}>About</button>
-        )
-    }
+  render() {
+    return (
+      <button onClick={jsHistory.pushState.bind(this, '/about')}>About</button>
+    )
+  }
 }
 
 class BtnTopics extends Component {
-    render() {
-        return (
-            <button onClick={jsHistory.pushState.bind(this, '/topics')}>Topics</button>
-        )
-    }
+  render() {
+    return (
+      <button onClick={jsHistory.pushState.bind(this, '/topics')}>Topics</button>
+    )
+  }
 }
 
 export default App
