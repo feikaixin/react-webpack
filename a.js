4b49013b1aa627144a5df5bc619a0e434822e679
feikaixin <954630886@qq.com>
2019-02-22 15:15:53 +0800

    简单实现react-router

--- a.js
+++ a.js
@@ -1,21 +1,31 @@
 import React,{Component} from 'react';
 import { BrowserRouter as Router,Link } from 'react-router-dom';
 import ReactDom from 'react-dom';
-
+import Index from './react-router/index';
 import { RouteWithSubRoutes } from './utils';
 import routes from './routes';
 
-export default class A extends Component{
-  render(){
-    return <div>
-      <Router>
-        <div>
-          <Link to="/B">to B</Link><br/>
-          <Link to="/C">to C</Link>
-          {routes.map((route, i) => <RouteWithSubRoutes key={i} {...route} />)}
-        </div>
-      </Router>
-    </div>
+// export default class A extends Component{
+//   render(){
+//     return <div>
+//       <Router>
+//         <div>
+//           <Link to="/B">to B</Link><br/>
+//           <Link to="/C">to C</Link>
+//           {routes.map((route, i) => <RouteWithSubRoutes key={i} {...route} />)}
+//         </div>
+//       </Router>
+//     </div>
+//   }
+// }
+
+export default class A extends Component {
+  render() {
+    return (
+      <div>
+        <Index />
+      </div>
+    )
   }
 }
 ReactDom.render(<A/>,document.querySelector("#btn"))
